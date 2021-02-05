ef52a844b2f61321bd541c370b53012635560241
sheyda888 <sheyda888@gmail.com>
2021-02-06 00:42:25 +0330

    menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FavoriteStarComponent } from './project/pages/rating/favorite-star/favorite-star.component';
+import { FavoriteStarComponent } from './project/common/rating/favorite-star/favorite-star.component';
 import { UserModule } from './project/pages/user/user.module';
-import { LayoutModule } from './project/pages/layout/layout.module';
 import { MatIconModule } from '@angular/material/icon';
+import { LayoutModule } from './project/layout/layout.module';
 
 
 
