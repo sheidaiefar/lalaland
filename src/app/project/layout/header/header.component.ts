cf76a9e347b948af07838c19f01b19cf9a395cf8
sheyda888 <sheyda888@gmail.com>
2021-02-06 13:57:52 +0330

    menu-toggling

--- src/app/project/layout/header/header.component.ts
+++ src/app/project/layout/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  @Output('menuToggle') menuToggle = new EventEmitter();
 
   constructor() { }
+
+  onMenuBtnclick(){
+    this.menuToggle.emit();
+  }
 
 }
