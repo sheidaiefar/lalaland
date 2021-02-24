d4bd2dfe7c8b2e57b3898951cb39d997094e5d18
sheyda888 <sheyda888@gmail.com>
2021-02-24 16:07:13 +0330

    ss

---
+++ src/app/project/common/slideshow/slideshow.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+@NgModule({
+  // imports: [BrowserModule, NgbModule],
+  // declarations: [NgbdCarouselBasic],
+  // exports: [NgbdCarouselBasic],
+  // bootstrap: [NgbdCarouselBasic]
+})
+export class SlideshowModule { }
