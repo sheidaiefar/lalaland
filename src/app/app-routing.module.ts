d9e93c64efce3604d770c7e29b9eb04a01cf19de
sheyda888 <sheyda888@gmail.com>
2021-02-27 16:43:33 +0330

    layouts base on routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccessLayoutModule } from './access-area/access-area-layout/access-layout.module';
+import { PublicLayoutComponent } from './public-area/public-area-layout/public-layout/public-layout.component';
 import { PublicHomeComponent } from './public-area/public-area-pages/public-home/public-home.component';
-import { HomeComponent } from './work-space/work-space-pages/content/home-page/home.component';
-
+import { WorkSpaceLayoutComponent } from './work-space/work-space-layout/work-space-layout/work-space-layout.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: PublicHomeComponent ,
+    component: PublicLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: PublicHomeComponent
+      }
+    ],
     pathMatch: 'full'
   },
   {
     path: 'public',
+    component: PublicLayoutComponent,
     loadChildren: () => import('./public-area/public-area.module').then(x => x.PublicAreaModule),
+
   },
   {
     path: 'access',
+    component: AccessLayoutModule,
     loadChildren: () => import('./access-area/access.module').then(x => x.AccessModule),
   },
   {
     path: 'work-space',
+    component: WorkSpaceLayoutComponent,
     loadChildren: () => import('./work-space/work-space.module').then(x => x.WorkSpaceModule),
-  },
-
+  }
 ];
 
 @NgModule({
